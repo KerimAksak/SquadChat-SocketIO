d0cdba2d866ca1813b003464fdbf0b613e68b6c5
kerimaksak <abdulkerim1783@hotmail.com>
2020-03-25 20:00:45 +0300

    Genel tasarım ayarlandı.

--- app/src/main/java/com/example/abdulkerimaksak/squadchat/MainActivity.kt
+++ app/src/main/java/com/example/abdulkerimaksak/squadchat/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.abdulkerimaksak.squadchat
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timerThread = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(1500)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@MainActivity, MessageActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+        timerThread.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
